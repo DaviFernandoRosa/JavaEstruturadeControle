db259d8b4ba88ebbb219365dcb10d711c27a3fe8
Davi Fernando Rosa <davir0514@gmail.com>
2021-05-08 21:56:48 -0300

    Arquivo

---
+++ EstruturaControle.java
@@ -0,0 +1,26 @@
+
+
+/**
+ *
+ * @author Davi
+ */
+public class EstruturaControle {
+    
+    
+   //exemplo comando if
+    //Observe que temos uma expressão comparando o valor1 com o valor2, no caso,
+    // expressão retornou um valor verdadeiro, o que fez com que a diretiva fosse
+    //executada.
+    
+    public static void main(String[]args){
+    
+    int valor1 = 5;
+    int valor2 = 10;
+    
+    if (valor1 > valor2){
+      System.out.println("Valor 1 é maior que Valor 2");
+    }
+    System.out.println("Soma dos valores: " + (valor1 + valor2));
+    
+    }  
+}
